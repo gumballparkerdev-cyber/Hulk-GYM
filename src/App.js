4a743153fa9e143cbd023011f2540c67bf339b94
JustZaki-booo <itachiravenx1@gmail.com>
2026-01-20 00:22:27 +0300

    Frontend final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter , Routes , Route , Link} from 'react-router-dom';
+import { BrowserRouter , Routes , Route} from 'react-router-dom';
 // Importing components for different pages
 import Nav from './components/Nav';
 import Homepage from './components/Homepage';
